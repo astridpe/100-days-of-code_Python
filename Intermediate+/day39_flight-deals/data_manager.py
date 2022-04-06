9299dc78ebde973b1daf1c46922ffa81c09ca139
astridpe <71065172+astridpe@users.noreply.github.com>
2022-04-06 12:24:23 +0200

    Worked with the flight-deals-project.

--- Intermediate+/day39_flight-deals/data_manager.py
+++ Intermediate+/day39_flight-deals/data_manager.py
@@ -1,3 +1,16 @@
+import requests
+from pprint import pprint
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        pass
+
+    def get_sheet_data(self):
+        # 1. Use the Sheety API to GET all the data in that sheet and print it out:
+        sheet_endpoint = "https://api.sheety.co/cafd093ede58c80ca9a465f6342401de/day39FlightDeals/prices"
+        response = requests.get(sheet_endpoint)
+        response.raise_for_status()
+        result = response.json()["prices"]
+        return result
+
+
--- Intermediate+/day39_flight-deals/flight_deals_main.py
+++ Intermediate+/day39_flight-deals/flight_deals_main.py
@@ -1,1 +1,7 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to
+# achieve the program requirements.
+
+from data_manager import DataManager
+
+sheet_data = DataManager().get_sheet_data()
+print(sheet_data)
