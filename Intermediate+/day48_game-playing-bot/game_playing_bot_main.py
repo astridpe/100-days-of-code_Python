08701ec5dad35f7c1ec1b0466380ec8c6ba894b9
astridpe <71065172+astridpe@users.noreply.github.com>
2022-05-22 15:31:56 +0200

    Discovering Selenium in Python with its different element-web-scraping tools.

--- Intermediate+/day48_game-playing-bot/game_playing_bot_main.py
+++ Intermediate+/day48_game-playing-bot/game_playing_bot_main.py
@@ -6,8 +6,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 chrome_driver_path = ""
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-driver.get("https://www.amazon.com/HOKA-ONE-Womens-Rincon-Running/dp/B099W3241F/ref=sr_1_1?keywords=hoka%2Bcarbon"
-           "%2Bx2%2Bwomens&qid=1651773006&sprefix=Hoka%2BCarbon%2BX%2B2%2Bw%2Caps%2C140&sr=8-1&th=1&psc=1")
+# driver.get("https://www.amazon.com/HOKA-ONE-Womens-Rincon-Running/dp/B099W3241F/ref=sr_1_1?keywords=hoka%2Bcarbon"
+#            "%2Bx2%2Bwomens&qid=1651773006&sprefix=Hoka%2BCarbon%2BX%2B2%2Bw%2Caps%2C140&sr=8-1&th=1&psc=1")
 
 # Classname:
 # price = driver. find_element(by=By.CLASS_NAME, value="apexPriceToPay")
@@ -27,4 +27,29 @@ driver.get("https://www.amazon.com/HOKA-ONE-Womens-Rincon-Running/dp/B099W3241F/
 
 
 # driver.close() <--- Closes a tab in the browser
+
+# PYTHON-EVENT CHALLENGE:
+# -Get the upcoming event list from the Python webpage and print it as a dictionary in the format: {0: {time: event:}}
+
+# driver.get("https://www.python.org/")
+#
+# events = {}
+# event_time = driver.find_elements(by=By.CSS_SELECTOR, value=".event-widget time")
+# event_name = driver.find_elements(by=By.CSS_SELECTOR, value=".event-widget  li a")
+#
+# for n in range(len(event_time)):
+#     events[n] = {"time": event_time[n].text,
+#                  "event": event_name[n].text,
+#                  }
+#
+# print(events)
+
+# WIKIPEDIA CHALLENGE:
+# - 1. Get the article number from the Wikipedia main page and print it out:
+
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+
+total_articles = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div[5]/div[1]/div[1]/div/div[3]/a[1]")
+print(total_articles.text)
+
 driver.quit()
