8c78fc1b66535554a1ac3bc1dbd9eac8929bb36b
astridpe <71065172+astridpe@users.noreply.github.com>
2022-05-30 20:40:58 +0200

    Completed the cookie-game-bot-challenge.

--- Cheatsheet/selenium.py
+++ Cheatsheet/selenium.py
@@ -1,1 +1,83 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.common.keys import Keys
+
+chrome_driver_path = ""
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+# driver.get("https://www.amazon.com/HOKA-ONE-Womens-Rincon-Running/dp/B099W3241F/ref=sr_1_1?keywords=hoka%2Bcarbon"
+#            "%2Bx2%2Bwomens&qid=1651773006&sprefix=Hoka%2BCarbon%2BX%2B2%2Bw%2Caps%2C140&sr=8-1&th=1&psc=1")
+
+# Classname:
+# price = driver. find_element(by=By.CLASS_NAME, value="apexPriceToPay")
+# print(price.text)
+
+# Id:
+# price = driver.find_element(by=By.ID, value="apexPriceToPay")
+
+# Name:
+# price = driver.find_element(by=By.NAME, value="q")
+
+# CSS-selector:
+# price = driver.find_element(by=By.CSS_SELECTOR, value=".apexPriceToPay span")
+
+# xpath: price = driver.find_element(by=By.XPATH, value='//*[@id="corePrice_desktop"]/div/table/tbody/tr/td[2]/span[
+# 1]/span[2]')
+
+
+# driver.close() <--- Closes a tab in the browser
+
+# PYTHON-EVENT CHALLENGE:
+# -Get the upcoming event list from the Python webpage and print it as a dictionary in the format: {0: {time: event:}}
+
+# driver.get("https://www.python.org/")
+#
+# events = {}
+# event_time = driver.find_elements(by=By.CSS_SELECTOR, value=".event-widget time")
+# event_name = driver.find_elements(by=By.CSS_SELECTOR, value=".event-widget  li a")
+#
+# for n in range(len(event_time)):
+#     events[n] = {"time": event_time[n].text,
+#                  "event": event_name[n].text,
+#                  }
+#
+# print(events)
+
+# WIKIPEDIA CHALLENGE:
+# - 1. Get the article number from the Wikipedia main page and print it out:
+
+# driver.get("https://en.wikipedia.org/wiki/Main_Page")
+#
+# total_articles = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div[5]/div[1]/div[1]/div/div[
+# 3]/a[1]") # print(total_articles.text)
+#
+# view_source = driver.find_element(by=By.LINK_TEXT, value="View source")
+# # view_source.click()
+#
+# search = driver.find_element(by=By.NAME, value="search")
+# search.send_keys("Python")
+# search.send_keys(Keys.ENTER)
+
+# Challenge - Fill out form using Selenium:
+driver.get("http://secure-retreat-92358.herokuapp.com/")
+fname = driver.find_element(by=By.NAME, value="fName")
+fname.send_keys("Astrid")
+
+lname = driver.find_element(by=By.NAME, value="lName")
+lname.send_keys("Pettersen")
+
+email = driver.find_element(by=By.NAME, value="email")
+email.send_keys("astridtesting282@gmail.com")
+
+submit = driver.find_element(by=By.TAG_NAME, value="button")
+submit.click()
+
+
+time.sleep(10)
+driver.quit()
+
+
 
--- Intermediate+/day48_game-playing-bot/game_playing_bot_main.py
+++ Intermediate+/day48_game-playing-bot/game_playing_bot_main.py
@@ -2,54 +2,50 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 chrome_driver_path = ""
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-# driver.get("https://www.amazon.com/HOKA-ONE-Womens-Rincon-Running/dp/B099W3241F/ref=sr_1_1?keywords=hoka%2Bcarbon"
-#            "%2Bx2%2Bwomens&qid=1651773006&sprefix=Hoka%2BCarbon%2BX%2B2%2Bw%2Caps%2C140&sr=8-1&th=1&psc=1")
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
-# Classname:
-# price = driver. find_element(by=By.CLASS_NAME, value="apexPriceToPay")
-# print(price.text)
+# Create a bot using Selenium and Python to click on the cookie as fast as possible:
+cookie = driver.find_element(by=By.ID, value="cookie")
 
-# Id:
-# price = driver.find_element(by=By.ID, value="apexPriceToPay")
+# Get upgrade item ids.
+items = driver.find_elements(by=By.CSS_SELECTOR, value="#store div")
+item_ids = [item.get_attribute("id") for item in items]
 
-# Name:
-# price = driver.find_element(by=By.NAME, value="q")
+# loop for 5 seconds:
+timeout = time.time() + 5
 
-# CSS-selector:
-# price = driver.find_element(by=By.CSS_SELECTOR, value=".apexPriceToPay span")
+# 5 min:
+five_min = time.time() + 60 * 5
 
-# xpath: price = driver.find_element(by=By.XPATH, value='//*[@id="corePrice_desktop"]/div/table/tbody/tr/td[2]/span[
-# 1]/span[2]')
+game_playing = True
 
+while game_playing:
+    cookie.click()
 
-# driver.close() <--- Closes a tab in the browser
+    # Every 5 seconds, check the right-hand pane to see which upgrades are affordable and purchase the most expensive
+    # one. You'll need to check how much money (cookies) you have against the price of each upgrade.
+    if time.time() > timeout:
+        # Check for best item, class '', i.e not 'grayed'
+        items_to_buy = driver.find_elements(
+            By.CSS_SELECTOR, 'div[class=""] b'
+        )
+        # Buy last item in list, aka most expensive
+        items_to_buy[:][-1].click()
 
-# PYTHON-EVENT CHALLENGE:
-# -Get the upcoming event list from the Python webpage and print it as a dictionary in the format: {0: {time: event:}}
+        # Add another 5 seconds until the next check
+        timeout = time.time() + 5
 
-# driver.get("https://www.python.org/")
-#
-# events = {}
-# event_time = driver.find_elements(by=By.CSS_SELECTOR, value=".event-widget time")
-# event_name = driver.find_elements(by=By.CSS_SELECTOR, value=".event-widget  li a")
-#
-# for n in range(len(event_time)):
-#     events[n] = {"time": event_time[n].text,
-#                  "event": event_name[n].text,
-#                  }
-#
-# print(events)
+    # After 5 minutes have passed since starting the game, stop the bot and print the "cookies/second". e.g. this is
+    # mine:
+    if time.time() > five_min:
+        game_playing = False
 
-# WIKIPEDIA CHALLENGE:
-# - 1. Get the article number from the Wikipedia main page and print it out:
-
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
-
-total_articles = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div[5]/div[1]/div[1]/div/div[3]/a[1]")
-print(total_articles.text)
+cookies_second = driver.find_element(by=By.ID, value="cps")
+print(cookies_second.text)
 
 driver.quit()
