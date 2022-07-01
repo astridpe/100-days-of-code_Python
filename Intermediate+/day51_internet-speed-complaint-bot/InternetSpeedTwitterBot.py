3b72c8d71101714e9490ccb8dde0a012aa517565
astridpe <71065172+astridpe@users.noreply.github.com>
2022-07-01 18:30:58 +0200

    Completed the automated-twitter-complaint-challenge.

--- Intermediate+/day51_internet-speed-complaint-bot/InternetSpeedTwitterBot.py
+++ Intermediate+/day51_internet-speed-complaint-bot/InternetSpeedTwitterBot.py
@@ -2,11 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-TWITTER_USERNAME = ""
-INTERNET_PROVIDER = ""
-TWITTER_EMAIL = ""
-TWITTER_PASSWORD = ""
-
 
 class InternetSpeedTwitterBot:
     def __init__(self, driver_path):
@@ -14,8 +9,10 @@ class InternetSpeedTwitterBot:
         self.down = 0
         self.up = 0
 
-    def get_internet_speed(self, website_link):
-        self.driver.get(website_link)
+    def get_internet_speed(self):
+        self.driver.get("https://www.speedtest.net")
+
+        time.sleep(3)
         go_button = self.driver.find_element(by=By.CLASS_NAME, value="js-start-test")
         go_button.click()
 
@@ -24,6 +21,50 @@ class InternetSpeedTwitterBot:
         self.down = float(self.driver.find_element(by=By.CLASS_NAME, value="download-speed").text)
         self.driver.close()
 
-    def complain(self, promised_up, promised_down, driver_path):
-        text = f"Hey, {INTERNET_PROVIDER}, why is my internet speed {self.down}down/{self.up}up when I pay for {promised_down}down/{promised_up}up"
+    def tweet_at_provider(self, promised_up, promised_down, twitter_email, twitter_password):
+        self.driver.get("https://twitter.com/login")
+
+        time.sleep(2)
+        email = self.driver.find_element(by=By.XPATH, value="/html/body/div/div/div/div["
+                                                            "1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div["
+                                                            "2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input")
+        email.send_keys(twitter_email)
+
+        next_button = self.driver.find_element(by=By.XPATH, value="/html/body/div/div/div/div["
+                                                                  "1]/div/div/div/div/div/div/div[2]/div["
+                                                                  "2]/div/div/div[2]/div[2]/div/div/div/div[6]/div")
+        next_button.click()
+        time.sleep(3)
+
+        password = self.driver.find_element(by=By.XPATH, value="/html/body/div/div/div/div["
+                                                               "1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div["
+                                                               "2]/div[2]/div[1]/div/div/div[2]/div/label/div/div["
+                                                               "2]/div[1]/input")
+        password.send_keys(twitter_password)
+
+        login_button = self.driver.find_element(by=By.XPATH, value="/html/body/div/div/div/div["
+                                                                   "1]/div/div/div/div/div/div/div[2]/div["
+                                                                   "2]/div/div/div[2]/div[2]/div[2]/div/div["
+                                                                   "1]/div/div/div/div")
+        login_button.click()
+
+        time.sleep(5)
+        tweet_compose = self.driver.find_element(by=By.XPATH,
+                                                 value="/html/body/div[1]/div/div/div[2]/header/div/div/div/div["
+                                                       "1]/div[3]/a/div/span/div/div/span/span")
+
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up " \
+                f"when I pay for {promised_down}down/{promised_up}up? "
+
+        tweet_compose.send_keys(tweet)
+        time.sleep(3)
+
+        tweet_button = self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div/div[1]/div["
+                                                                   "2]/div/div/div/div/div/div[2]/div["
+                                                                   "2]/div/div/div/div[3]/div/div["
+                                                                   "1]/div/div/div/div/div[2]/div[3]/div/div/div["
+                                                                   "2]/div/div/span/span")
+        tweet_button.click()
+        time.sleep(2)
+        self.driver.quit()
 
--- Intermediate+/day51_internet-speed-complaint-bot/complaint_main.py
+++ Intermediate+/day51_internet-speed-complaint-bot/complaint_main.py
@@ -3,15 +3,14 @@ from selenium.webdriver.chrome.service import Service
 
 PROMISED_DOWN = 250
 PROMISED_UP = 250
-CHROME_DRIVER_PATH = "/Users/astridpettersen/Documents/Python/chromedriver"
-WEBSITE = "https://www.speedtest.net"
+CHROME_DRIVER_PATH = ""
 TWITTER_EMAIL = ""
 TWITTER_PASSWORD = ""
 
 twitter_bot = InternetSpeedTwitterBot(driver_path=CHROME_DRIVER_PATH)
-internet_speed = twitter_bot.get_internet_speed(WEBSITE)
+internet_speed = twitter_bot.get_internet_speed()
 
 if twitter_bot.up < PROMISED_UP or twitter_bot.down < PROMISED_DOWN:
-    twitter_bot.complain(PROMISED_UP, PROMISED_DOWN, CHROME_DRIVER_PATH)
+    twitter_bot.tweet_at_provider(PROMISED_UP, PROMISED_DOWN, TWITTER_EMAIL, TWITTER_PASSWORD)
 
 
