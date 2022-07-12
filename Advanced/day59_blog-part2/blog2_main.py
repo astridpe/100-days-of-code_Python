32e3a212f7920b43cf14a922df518c6de49196ac
astridpe <71065172+astridpe@users.noreply.github.com>
2022-07-12 18:46:26 +0200

    Completed the html-form implementing of yesterdays blog-project.

--- Advanced/day59_blog-part2/blog2_main.py
+++ Advanced/day59_blog-part2/blog2_main.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+
+MY_EMAIL = ""
+PASSWORD = ""
 
 response = requests.get("https://api.npoint.io/1e28b176e629a3b4f155")
 post_data = response.json()
@@ -27,9 +31,29 @@ def get_post(num):
     return render_template("post.html", post=requested_post)
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+# day60 implementing html-forms:
+@app.route("/contact", methods=["GET", "POST"])
+def receive_data():
+    if request.method == "POST":
+        data = request.form
+        mail = f"Name: {data['name']} \n" \
+               f"Email: {data['email']} \n" \
+               f"Phone: {data['phone']} \n" \
+               f"Message: {data['message']} \n"
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL,
+                                to_addrs=MY_EMAIL,
+                                msg=f"Subject:New Message\n\n {mail}"
+                                )
+
+        message = "Successfully sent your message"
+        message_sent = True
+        return render_template("contact.html", message=message, message_sent=message_sent)
+    else:
+        return render_template("contact.html")
 
 
 if __name__ == "__main__":
